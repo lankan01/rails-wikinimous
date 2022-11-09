52144ba6a35623d4c69edd91bcfbd94c9b0d7e1c
Jega Pradeeba <pjega92@gmail.com>
2022-11-09 17:01:05 +0800

    wikinonymous with rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  articles = Article.new(
+    title: Faker::Games::StreetFighter.stage,
+    content: Faker::Games::StreetFighter.quote
+  )
+  articles.save
+end
